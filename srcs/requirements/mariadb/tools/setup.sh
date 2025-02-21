7ec92da18355dcf01f10d2014c8a020afef00246
vlomakin <you@example.com>
2025-02-21 18:34:48 +0400

    fixed in VM

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
 
-echo "MYSQL_NAME=${MYSQL_NAME}, MYSQL_USER=${MYSQL_USER}, MYSQL_PASSWORD=${MYSQL_PASSWORD}"
+echo "MYSQL_NAME=${MYSQL_NAME}, MYSQL_USER=${MYSQL_USER}, MYSQL_PASSWORD=${MYSQL_PASSWORD}, MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}"
 
-if [ -d "/var/lib/mysql/wordpress" ]; then
-        mysqld --user=mysql
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing database..."
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql
 fi
 
-DB="USE mysql;
+DB_SETUP="
+USE mysql;
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user WHERE User='';
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${MYSQL_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
 FLUSH PRIVILEGES;
 DROP USER IF EXISTS '${MYSQL_USER}'@'%';
 CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%';
-FLUSH PRIVILEGES;"
+FLUSH PRIVILEGES;
+"
 
-echo "${DB}"
+echo "${DB_SETUP}" > /tmp/setup.sql
 
-echo "${DB}" | /usr/bin/mysqld --user=mysql --bootstrap
+/usr/bin/mysqld --user=mysql --bootstrap < /tmp/setup.sql
+rm -f /tmp/setup.sql
 
-rm -f /tmp/create_db.sql
-mysqld --user=mysql
-
+exec mysqld --user=mysql --socket=/run/mysqld/mysqld.sock
